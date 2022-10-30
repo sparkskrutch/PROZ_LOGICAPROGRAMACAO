6347c5464d85a76264db7304794c4ee404a091ab
Melissa Gouveia <melissa.limaft@gmail.com>
2022-10-29 21:30:49 -0300

    sparks

---
+++ problema_joao2.py
@@ -0,0 +1,13 @@
+def calculadoraimc(peso, altura):
+    imc = peso/(altura*altura) * 10000
+    if(imc <= 18.5): return "magreza"
+    elif(imc > 18.5) and (imc <= 24.9): return "saudavel"
+    elif(imc > 25) and (imc <= 29.9): return "Sobrepeso"
+    elif(imc > 30) and (imc <= 34.9): return "Obesidade grau 1"
+    elif(imc > 35) and (imc <= 39.9): return "Obesidade severa grau 2"
+    else: return "Obesidade morbida grau 3"
+
+peso = 60
+altura = 159
+resultado = calculadoraimc(peso,altura)
+print(resultado)
