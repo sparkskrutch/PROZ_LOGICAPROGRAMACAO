049fbc0e5636c1014490fa6f2ae53ae81a19a177
Melissa Gouveia <melissa.limaft@gmail.com>
2022-11-01 13:27:03 -0300

    .

---
+++ (revisar)teste2.py
@@ -0,0 +1,8 @@
+for andar in range(20, 13, -1):
+  print("Andar n°", andar)
+for andar in range(12, 0, -1):
+  print("Andar n°", andar)
+  
+for andar in range(20, 0, -1):
+  if andar != 13:
+    print("Andar n°", andar)
