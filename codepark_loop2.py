ba246eb4c1096b5093b251a41477a923acbdc358
Melissa Gouveia <melissa.limaft@gmail.com>
2022-10-31 23:43:50 -0300

    codepark_aula2 (loop)

---
+++ codepark_loop2.py
@@ -0,0 +1,6 @@
+for floor in range(20,0,-1):
+    if (floor == 13):
+        continue
+    else:
+        (floor == 21)
+    print(floor)
---
+++ codepark_loop3.py
@@ -0,0 +1,8 @@
+i = 0
+while (i < 20):
+    i = i + 1
+    if (i == 13):
+        continue
+    elif (i == 21):
+        break
+    print(i)
