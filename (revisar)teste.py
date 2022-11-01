bd04dc230f6dc67afc3e27ec76a24469509c5be9
Melissa Gouveia <melissa.limaft@gmail.com>
2022-11-01 13:27:11 -0300

    .

---
+++ (revisar)teste.py
@@ -0,0 +1,6 @@
+andar2 = 20
+while andar2 > 0:
+  print(f'{andar2}° andar')
+  andar2 -= 1
+  if andar2 != 13:
+    continue
--- teste.py
+++
@@ -1,9 +0,0 @@
-i = 20
-while (i == 0):
-    i = i + 1
-    if (i == 13):
-        continue
-    if (i == 21):
-        break
-    print(i)
-    
