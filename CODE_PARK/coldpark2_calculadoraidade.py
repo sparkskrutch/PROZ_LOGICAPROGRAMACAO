ff97aa284eed80dae365edbe29d4c888a0069a43
Melissa Gouveia <melissa.limaft@gmail.com>
2022-11-03 15:08:51 -0300

    .

--- CODE_PARK/coldpark2_calculadoraidade.py
+++ CODE_PARK/coldpark2_calculadoraidade.py
@@ -1,13 +1,13 @@
-contador = 0
-while():
+idadeValida = False
+while(idadeValida == False):
     try:
         nomeCompleto = str(input("Nome Completo: "))
         anoNascimento = int(input("Ano nascimento: "))
         idade = 2022 - anoNascimento
-
         if (anoNascimento < 1921):
             print("Idade inválida")
         else:
+            idadeValida = True
             print("-"*40)
             print("Sua idade")
             print("Nome Completo:", nomeCompleto)
