b683acfe73320cedfed254b387c3a8c64e4c3aca
Melissa Gouveia <melissa.limaft@gmail.com>
2022-11-04 20:51:55 -0300

    .

---
+++ CODE_PARK/coldpark2_calculadoraidade_v2.py
@@ -0,0 +1,23 @@
+from datetime import date
+
+def calculadoraIdade():
+    teste = False
+    try:
+        while (teste == False):
+            nome = str(input("Nome completo: "))
+            current_date = date.today()
+            data_nascimento = int(input("Ano nascimento:"))
+            data_atual = current_date.year
+            idade = data_atual - data_nascimento
+            if (data_nascimento < 1922):
+                print("Insira um ano maior que 1922.")
+            else:
+                teste = True
+                print("-"*40)
+                print("Nome:", nome)
+                print("Sua idade:", idade)
+                
+    except:
+        print("Você não digitou os dados corretamente.")
+
+calculadoraIdade()
--- EXERCICIOS_DISCORD/exercicio_tabuada.py
+++ EXERCICIOS_DISCORD/exercicio_tabuada.py
@@ -1,4 +1,4 @@
 tabuada=int(input("De qual número você quer a tabuada? "))
 
 for count in range(10):
-    print("%d x %d = %d" % (tabuada, count+1, tabuada*(count+1)))
+    print("%d x %d = %d" % (tabuada, count+1, tabuada*(count+1))) #(%d) é um placeholder
